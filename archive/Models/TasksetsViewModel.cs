73448cceacc657ac17672b8d74f6d8109e311c7a
Beniamin Jędrychowski <blomex.bloomex@gmail.com>
2019-03-16 23:39:01 +0100

    updated model accordingly

--- archive/Models/TasksetsViewModel.cs
+++ archive/Models/TasksetsViewModel.cs
@@ -6,11 +6,13 @@ namespace archive.Models
     {
         public IEnumerable<TasksetViewModel> Tasksets { get; }
         public string CourseName { get; }
+        public int Year { get; }
 
-        public TasksetsViewModel(IEnumerable<TasksetViewModel> tasksets, string courseName)
+        public TasksetsViewModel(IEnumerable<TasksetViewModel> tasksets, string courseName, int year)
         {
             Tasksets = tasksets;
             CourseName = courseName;
+            Year = year;
         }
     }
 }
