2d115b30c068cb05cb4df4327d8f54e77e0ac3ac
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-03-17 18:50:24 +0100

    migrations

---
+++ archive/Migrations/20190317174957_Migration2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace archive.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Courses_Name",
+                table: "Courses",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Courses_Name",
+                table: "Courses");
+        }
+    }
+}
