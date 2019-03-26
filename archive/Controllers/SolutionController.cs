a399b0ffca3310a76b05c055743ac9749c486f3a
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2019-03-26 23:20:41 +0100

    Zalążek kontrolera SolutionsController
    * Zmodyfikowane routowanie, żeby dało się wejść w widok rozwiązania przez "solution/{id}"
    * Dodany zalążek klasy z modelem danych oraz zalążek kontrolera

---
+++ archive/Controllers/SolutionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using archive.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace archive.Controllers
+{
+    public class SolutionController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public SolutionController(ILogger<SolutionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Show(int solutionId)
+        {
+            _logger.LogDebug($"Requested solution for {solutionId}");
+            SolutionViewModel model = new SolutionViewModel();
+            model.TaskName = "Zadanie " + solutionId;
+            return View("Show", model);
+        }
+    }
+}
---
+++ archive/Models/SolutionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace archive.Models
+{
+    public class SolutionViewModel
+    {
+        public string TaskName { get; set; }
+    }
+}
