b9bec27db4e6d06cb0ccd0480455a195c237d725
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-04-01 23:23:46 +0200

    Dodawanie egzaminow i zadan do nich

---
+++ archive/Models/CreateTaskViewModel.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using archive.Data.Entities;
+using archive.Data.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace archive.Models
+{
+    [Display(Name = "Dodaj zadanie")]
+    public class CreateTaskViewModel
+    {
+        [Display(Name = "Nazwa")]
+        [Required] 
+        public string Name { get; set; }
+
+        [Display(Name = "Treść")] 
+        public string Content { get; set; }
+
+        [Display(Name = "Zbiór zadań")]
+        [Required]
+        public int TasksetId { get; set; }
+
+        public List<SelectListItem> Tasksets { get; }
+
+        [Display(Name = "Przedmiot")]
+        public List<SelectListItem> Courses { get; }
+
+        public CreateTaskViewModel()
+        {
+        }
+
+        public CreateTaskViewModel(List<Taskset> tasksets)
+        {
+            var coursesNames = tasksets
+                .Select(t => t.Course.Name)
+                .Distinct()
+                .OrderBy(n => n)
+                .ToList();
+
+            Courses = coursesNames
+                .Select(n => new SelectListItem(n, n))
+                .ToList();
+
+            var courseGroups = coursesNames
+                .Select(n => new SelectListGroup {Name = n})
+                .ToDictionary(g => g.Name);
+
+            Tasksets = tasksets
+                .OrderBy(t => t.Course.Name)
+                .ThenByDescending(t => t.Year)
+                .ThenBy(t => t.Name)
+                .Select(t => new SelectListItem
+                {
+                    Text = $"({t.Year}) {t.Name}", Value = t.Id.ToString(),
+                    Group = courseGroups[t.Course.Name]
+                })
+                .ToList();
+        }
+    }
+}
