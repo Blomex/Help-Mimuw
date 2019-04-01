ffaa82197f64d0495a49db05b5c8e339a5b0fafa
Beniaminb <Beniamin@BENIAMIN>
2019-04-01 14:20:17 +0200

    szkic wyswietlania rozwiazan

---
+++ archive/Services/ISolutionService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SolutionEntity = archive.Data.Entities.Solution;
+
+namespace archive.Services
+{
+    public interface ISolutionService
+    {
+        Task<List<SolutionEntity>> FindForTaskAsync(string taskName, string tasksetName, string courseName, int tasksetYear);
+
+    }
+}
