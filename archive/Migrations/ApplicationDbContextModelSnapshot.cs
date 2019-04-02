92244ecb2b7776875827d59435fc61e9284fd4d3
Wojciech Matusiak <wm382710@students.mimuw.edu.pl>
2019-04-02 13:42:47 +0200

    Migracje z repo nie były w stanie nałożyć się na czystą bazę, więc są nowe

--- archive/Migrations/ApplicationDbContextModelSnapshot.cs
+++ archive/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace archive.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
-                .HasAnnotation("ProductVersion", "2.2.0-rtm-35687")
+                .HasAnnotation("ProductVersion", "2.2.3-servicing-35854")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
