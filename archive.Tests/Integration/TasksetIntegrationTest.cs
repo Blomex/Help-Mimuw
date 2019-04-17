77d663d5fb0eb7a54eedf094dac6e6681e5d70d3
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-04-17 10:07:00 +0200

    testy wyswietlania egzaminow

--- archive.Tests/Integration/TasksetIntegrationTest.cs
+++ archive.Tests/Integration/TasksetIntegrationTest.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using archive.Data;
+using archive.Data.Entities;
 using archive.Data.Enums;
 using archive.Models.Taskset;
 using AngleSharp.Dom;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
+using Task = System.Threading.Tasks.Task;
 
 namespace archive.Tests.Integration
 {
@@ -28,6 +30,55 @@ namespace archive.Tests.Integration
         }
 
         [Test]
+        public async Task ShowTasksets()
+        {
+            using (var scope = _factory.Server.Host.Services.CreateScope())
+            {
+                // Arrange
+                var repository = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var name1 = Guid.NewGuid().ToString();
+                var name2 = Guid.NewGuid().ToString();
+                var taskset1 = new Taskset {Name = name1, Type = TasksetType.Exam, Year = 2000, CourseId = 1};
+                var taskset2 = new Taskset {Name = name2, Type = TasksetType.Test, Year = 2001, CourseId = 1};
+                repository.Tasksets.Add(taskset1);
+                repository.Tasksets.Add(taskset2);
+                repository.SaveChanges();
+                
+                // Act
+                var tasksetsResponse = await _client.GetAsync("/Taskset/Index/1");
+                var tasksetsHtmlDocument = await HtmlHelpers.GetDocumentAsync(tasksetsResponse);
+                Assert.AreEqual(HttpStatusCode.OK, tasksetsResponse.StatusCode);
+
+                // Assert
+                var main = tasksetsHtmlDocument.GetElementsByTagName("main");
+                Assert.AreEqual(1, main.Length);
+
+                var expectedTasksets = repository.Tasksets
+                    .Where(t => t.CourseId == 1)
+                    .Select(t => t.Name)
+                    .ToList();
+                repository.Remove(taskset1);
+                repository.Remove(taskset2);
+                repository.SaveChanges();
+                
+                var obtainedTasksets = new List<string>();
+                foreach (var a in main.Children("li a"))
+                {
+                    obtainedTasksets.Add(a.TextContent);
+                }
+                
+                obtainedTasksets.Sort();
+                expectedTasksets.Sort();
+
+                for (int i = 0; i < obtainedTasksets.Count; ++i)
+                {
+                    Assert.AreEqual(expectedTasksets[i], obtainedTasksets[i].Trim());
+                }
+            }
+        }
+
+        [Test]
+        [Ignore("Require being logged -- unsupported yet.")]
         public async Task CreateTaskset()
         {
             // Arrange
@@ -38,23 +89,28 @@ namespace archive.Tests.Integration
                     AllowAutoRedirect = false,
                 });
 
-            
+
             // Act
             var redirect = await client.PostAsJsonAsync("/Taskset/Create",
-                new CreateTasksetViewModel {Type = TasksetType.Other.ToString(), Name = name, CourseId = 1});
+                new CreateTasksetViewModel
+                    {Type = TasksetType.Other.ToString(), Year = 2000, Name = name, CourseId = 1});
 
             // Assert
             Assert.AreEqual(HttpStatusCode.OK, redirect.StatusCode);
             Assert.True(redirect.Headers.Location.OriginalString.StartsWith("/Taskset/Index"));
             using (var scope = _factory.Server.Host.Services.CreateScope())
             {
-                
                 var repository = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//                repository.Users.
                 var added = repository.Tasksets.Where(t => t.Name == name).ToList();
+
                 Assert.Equals(1, added.Count);
+                repository.Tasksets.Remove(added[0]);
+                repository.SaveChanges();
+
                 Assert.Equals(name, added[0].Name);
-//                Assert.Equals()
+                Assert.Equals(1, added[0].CourseId);
+                Assert.Equals(TasksetType.Exam, added[0].Type);
+                Assert.Equals(2000, added[0].Year);
             }
         }
     }
