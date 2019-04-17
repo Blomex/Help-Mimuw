53b81e0e407a7eb706d688d5462271ec470a30dc
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-04-17 20:44:03 +0200

    zmiana nazwy kolumny Comments.content na Comments.Content

---
+++ archive/Migrations/20190417184300_Content-column-in-comments.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace archive.Migrations
+{
+    public partial class Contentcolumnincomments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "content",
+                table: "Comments",
+                newName: "Content");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Comments",
+                newName: "content");
+        }
+    }
+}
