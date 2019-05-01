f6778f909e72a1697e9e6cc329081cb5c912ca63
Wojciech Matusiak <saphir2357@gmail.com>
2019-05-01 19:31:02 +0200

    Poprawione migracje

---
+++ archive/Migrations/20190501171246_UserProfileChanges.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace archive.Migrations
+{
+    public partial class UserProfileChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HomePage",
+                table: "AspNetUsers",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastActive",
+                table: "AspNetUsers",
+                type: "timestamp",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Avatars",
+                columns: table => new
+                {
+                    ApplicationUserId = table.Column<string>(nullable: false),
+                    Image = table.Column<byte[]>(maxLength: 3145728, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avatars", x => x.ApplicationUserId);
+                    table.ForeignKey(
+                        name: "FK_Avatars_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avatars");
+
+            migrationBuilder.DropColumn(
+                name: "HomePage",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastActive",
+                table: "AspNetUsers");
+        }
+    }
+}
