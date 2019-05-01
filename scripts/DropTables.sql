376d4c7e526e7ac3fc8fc7add8514d5242f31e28
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-05-01 20:42:31 +0200

    skrypt do usuwania wszystkich tabel -- przydatne gdy heroku się zwali, bo nie można zrobić drop database

---
+++ scripts/DropTables.sql
@@ -0,0 +1,16 @@
+drop table "AspNetRoleClaims" cascade; 
+drop table "AspNetRoles" cascade; 
+drop table "AspNetUserClaims" cascade; 
+drop table "AspNetUserLogins" cascade; 
+drop table "AspNetUserRoles" cascade; 
+drop table "AspNetUserTokens" cascade; 
+drop table "AspNetUsers" cascade; 
+drop table "Avatars" cascade; 
+drop table "Comments" cascade; 
+drop table "Courses" cascade; 
+drop table "Ratings" cascade; 
+drop table "Solutions" cascade;
+drop table "SolutionsVersions" cascade;
+drop table "Tasks" cascade;
+drop table "Tasksets" cascade;
+drop table "__EFMigrationsHistory"
