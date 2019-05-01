ae9f548c8f7675f965b47914ddcba7368d702dc9
Wojciech Matusiak <saphir2357@gmail.com>
2019-05-01 13:10:14 +0200

    Mozliwosc edycji tresci zadan
    * Moderatorzy moga teraz edytowac zadania

---
+++ archive/Models/Task/TaskEditModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace archive.Models.Task
+{
+    public class TaskEditModel
+    {
+        public int Id { get; set; }
+
+        public Data.Entities.Taskset Taskset { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [Display(Name = "Nazwa zadania")]
+        public string NewName { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [Display(Name = "Treść zadania")]
+        public string NewContent { get; set; }
+    }
+}
