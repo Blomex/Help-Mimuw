5b587670fa1c0da6c5a9d2589551ff4192fa8c61
Beniamin Jędrychowski <blomex.bloomex@gmail.com>
2019-05-14 23:02:06 +0200

    Filtrowanie (#86)
    * pierwsze filtrowanie
    
    * próba poprawy przez dodanie include
    
    * zmiana formularza na GET
    
    * Poprawki z komentarzy
    
    * Problem z rokiem zerowym w formularzu poprawiony przez zamiane yearTo z 0 na 3000, mam nadzieję że nie będziemy obsługiwać systemu dłużej niż tysiąćlecie, a nie mam innego pomysłu jak to poprawić
    
    * dodanie wartości domyślnych w modelu IndexViewModel aby przez przypadek nie filtrować lata od 0 do 0
    
    * zamiast 8 wcięć 2 zapytania sql

--- archive/Models/Taskset/IndexViewModel.cs
+++ archive/Models/Taskset/IndexViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace archive.Models.Taskset
 {
@@ -6,6 +7,16 @@ namespace archive.Models.Taskset
     {
         public Data.Entities.Course Course { get; set; }
         public IEnumerable<Data.Entities.Taskset> Tasksets { get; set;}
+
+        [Display(Name = "Czy posiada zadania?")]
+        public bool haveTasks {get; set;}
+        [Display(Name = "Czy posiada rozwiązane zadania?")]
+        public bool haveSolutions {get; set;}
+        [Display(Name = "Egzaminy od:")]
+        public int yearFrom {get; set;} = 2010;
+        [Display(Name = "Egzaminy do:")]
+        public int yearTo {get; set;} = 2020;
+
     }
 
 }
