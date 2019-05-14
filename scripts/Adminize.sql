f56ce847562559385a546559c4df124e577843b0
Piotr Krzywicki <krzywicki.ptr@gmail.com>
2019-05-14 22:54:16 +0200

    dodanie wszystkich ról w skrypcie adminize

--- scripts/Adminize.sql
+++ scripts/Adminize.sql
@@ -2,12 +2,18 @@ CREATE OR REPLACE FUNCTION Adminize() RETURNS void AS
 $$
 DECLARE
     imperator_id TEXT;
+    moderator_id TEXT;
+    trusted_user_id TEXT;
     user_id TEXT;
 BEGIN
     SELECT "Id" FROM "AspNetRoles" WHERE "Name"='IMPERATOR' INTO imperator_id;
+    SELECT "Id" FROM "AspNetRoles" WHERE "Name"='MODERATOR' INTO moderator_id;
+    SELECT "Id" FROM "AspNetRoles" WHERE "Name"='TRUSTED_USER' INTO trusted_user_id;
 
     FOR user_id IN SELECT "Id" FROM "AspNetUsers" LOOP
         INSERT INTO "AspNetUserRoles" VALUES (user_id, imperator_id) ON CONFLICT DO NOTHING;
+        INSERT INTO "AspNetUserRoles" VALUES (user_id, trusted_user_id) ON CONFLICT DO NOTHING;
+        INSERT INTO "AspNetUserRoles" VALUES (user_id, moderator_id) ON CONFLICT DO NOTHING;
     END LOOP;
     RETURN;
 END
