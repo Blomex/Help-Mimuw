71f38eb3a1668eeb2b69058048a1d4e259eb01c5
m-soltan <ms387544@students.mimuw.edu.pl>
2019-05-29 22:49:20 +0200

    magiczna linijka dodająca komunikaty o błędach

--- archive/Startup.cs
+++ archive/Startup.cs
@@ -93,6 +93,7 @@ namespace archive
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+        	app.UseStatusCodePages();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
