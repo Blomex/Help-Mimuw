4f88a7eee0fb4cbb6a80fdec2e6afc13ca2343f6
krzywickiptr <40469966+krzywickiptr@users.noreply.github.com>
2019-05-29 22:41:03 +0200

    Tagi (#121)
    * naprawienie dodawania i wyswietlania
    
    * naprawienie migracji
    
    * usuniecie redundantnych calli do bd

---
+++ archive/Migrations/20190529183724_Tags-view-and-edit.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace archive.Migrations
+{
+    public partial class Tagsviewandedit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    TaskId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tags_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_TaskId",
+                table: "Tags",
+                column: "TaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
